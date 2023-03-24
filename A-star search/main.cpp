24ab76368c7b7733ad6c3f554455792f2e54d4e7
Pedro <pedro.csa.2002@gmail.com>
2023-03-24 09:36:02 -0300

    adding stack logic and final changes

--- A-star search/main.cpp
+++ A-star search/main.cpp
@@ -11,30 +11,24 @@
 
 using namespace std;
 
-Node* createNode(string name){
-
-    //cout << name << "  ->  Node created" << endl;
-    Node* node = new Node(name);
-    return node;
-
-}
-
+Node* createNode(string name);
 void fillCityWithBuffer(char *city, char *buffer);
 int getDistanceFromBuffer(int index, char *buffer);
 void deleteNode(Node* node);
 
 int main(int argc, char* argv[]){
-    //variables
+
+    // Variables
     char graph_file_name[] = "Grafo.txt";
     char heuristics_file_name[]  = "Heuristica.txt";
-    FILE *file_pointer;
     char next_ch;
     char city_origin[BUFFER_SIZE] = "______________", city_target[BUFFER_SIZE] = "______________", input_buffer[BUFFER_SIZE] = "______________";
     int distance = 0;
     int index = 0, state = READ_ORIGIN_CITY_STATE;
+    FILE *file_pointer;
     map<string /*City Name*/, Node* /*City Node*/> nodes;
 
-    //graph file
+    // Grafos.txt
     file_pointer = fopen(graph_file_name, "r");
     if(file_pointer == NULL)
     {
@@ -42,8 +36,6 @@ int main(int argc, char* argv[]){
     }
     else
     {
-        //handle input
-
         //find new roads
         while((next_ch = fgetc(file_pointer)) != -1)
         {
@@ -88,13 +80,6 @@ int main(int argc, char* argv[]){
                 it_first->second->conections.insert(make_pair(it_second->second, distance));
                 it_second->second->conections.insert(make_pair(it_first->second, distance));
 
-                ////////////////////////////////////////////////////////////////////////////
-                /*
-                    There it is, here we can get all info about roads
-                */
-                //printf("%s;%s;%d\n", city_origin, city_target, distance);
-                ////////////////////////////////////////////////////////////////////////////
-
                 //cleaning index, distance, city origin and city target
                 index = 0;
                 distance = 0;
@@ -111,7 +96,7 @@ int main(int argc, char* argv[]){
     }
     fclose(file_pointer);
 
-    //heuristics file
+    // Heurística.txt
     file_pointer = fopen(heuristics_file_name, "r");
     if(file_pointer == NULL)
     {
@@ -119,8 +104,6 @@ int main(int argc, char* argv[]){
     }
     else
     {
-        //handle input
-
         //in heuristics, target city is always Bucareste
         sprintf(city_target, "Bucareste");
 
@@ -141,17 +124,8 @@ int main(int argc, char* argv[]){
                 distance = getDistanceFromBuffer(index, input_buffer);
 
                 //save origin city and distance in node
-                //cout << "H( " << city_origin << " ) : " << distance << endl;
                 auto it_first = nodes.find(city_origin);
                 it_first->second->straight_line_to_bucarest = distance;
-
-                ////////////////////////////////////////////////////////////////////////////
-                /*
-                    There it is, here we can get all info about heuristics
-                */
-                //printf("%s ate %s sao %d\n", city_origin, city_target, distance);
-                //printf("%s;%d\n", city_origin, distance);
-                ////////////////////////////////////////////////////////////////////////////
 
                 //cleaning index, distance and city origin
                 index = 0;
@@ -169,147 +143,99 @@ int main(int argc, char* argv[]){
 
     }
 
-    // Printar todas as conexoes de cada cidade para checar se o salvamento das conexoes está correto
-
-
     // A* Search Logic
-
-    int node_g = 0 /*Gasto*/, node_h = 0; /*Heurística*/;
-    int node_f = 0; // g + h -> Disntacia entre cidade origem e cidade alvo + distancia da cidade alvo até Bucareste
-    int smaller_f = INT_MAX; // Variável para salvar o menor f dentre os nós que foram abertos
+    int smaller_f = INT_MAX; // Variable to store the smaller F(x) value
     bool isDestiny = false;
-
-    
-    map<string, pair<Node*, int /*G(x)*/>> opened_nodes;  // Ex: Arad : {Arad Node Object, F(Arad)}
-    list<string> cities; // Salvar as cidades que podem ser visitadas a partir da cidade atual
-
-    // Salvando configurações iniciais de Arad
-    auto it_Arad = nodes.find("Arad");
-    string city = "Arad"; it_Arad->second->visited = true; // Salvando Arad como a cidade de partida
-    it_Arad->second->f_x = it_Arad->second->straight_line_to_bucarest; // Setando o F(x) de Arad que é igual a H(x)
-    opened_nodes["Arad"] = pair<Node*, int>(it_Arad->second, it_Arad->second->f_x); // Salvando Arad em opened_nodes
-
     string destiny = "Bucareste";
     string next_city;
+    map<string /*City Name*/, Node*> opened_nodes;
 
+    // Saving Arad's info
+    auto it_Arad = nodes.find("Arad");
+    string city = "Arad"; it_Arad->second->visited = true; // Savaing Arad as the origin city
+    it_Arad->second->f_x = it_Arad->second->straight_line_to_bucarest; // Setting Arad's F(x) = H(x)
+    opened_nodes["Arad"] = it_Arad->second; // Saving Arad in opened_nodes
+
+    // While I have not reached Bucareste using the best way possible
     while(!isDestiny){
 
-        auto present_city = nodes.find(city);
-        auto present_city_connections = present_city->second->conections;
         smaller_f = INT_MAX;
+        auto present_city = nodes.find(city);
+        auto present_city_connections = present_city->second->conections; // Getting all the conections of the present city
 
-        // Printando as conexões da cidade atual
-        cout << "As conexoes de " << city << " sao: ";
-        for (auto it = present_city_connections.begin(); it != present_city_connections.end(); ++it){
-            cout << it->first->name << " | ";
-        }
-
-        cout << endl;
-
-
-        // Adiconando as conexões da cidade atual em um map de nós abertos junto com o F(x) de cada uma das conexões
+        // Adding the conections of the present city in the opened_nodes map
         for (auto it = present_city_connections.begin(); it != present_city_connections.end(); ++it){
 
-            //TO-DO: Acrescentar lógica de salvar o Nó anterior de cada Nó filho
-            it->first->previous_node = present_city->second->name;
+            // Saving the previous city visited
+            if(it->first->visited == false) { it->first->previous_node = present_city->second->name; }
             
-            // Calculando G(x) e F(x) de cada Nó
-            it->first->g_x = it->second /*G(x)*/ + present_city->second->g_x /*G(x) do Nó anterior*/;
+            // Calculating G(x) and F(x) of each node
+            it->first->g_x = it->second /*G(x)*/ + present_city->second->g_x /*G(x) of the previous node*/;
             it->first->f_x /*F(x)*/ = it->first->g_x /*G(x)*/ + it->first->straight_line_to_bucarest /*H(x)*/;
-            cout << "F( " << it->first->name << " ) : " << it->first->g_x << " + " << it->first->straight_line_to_bucarest << " = " << it->first->f_x << endl;
 
-            // Para cada Nó aberto será salvo seu nome junto com seu objeto e seu F(x)
+            // Inserting the opened nodes in the map
             if(it->first->visited == false){
-                opened_nodes.insert(pair<string, pair<Node*,int>>(it->first->name, pair<Node*, int>(it->first, it->first->f_x)));
+                opened_nodes.insert(pair<string, Node*>(it->first->name, it->first));
             }
 
         }
 
-        cout << "Estou em " << city << " e posso ir para :" << endl /*Todos os nós que estão em opened_nodes que ainda não foram visitados*/;
+        cout << "Estou em " << city << " e posso ir para :" << endl;
 
         for (auto it = opened_nodes.begin(); it != opened_nodes.end(); ++it){
-            if(it->second.first->visited == false) { cout << it->second.first->name /*Nome do Nó*/ << " com F(x) = " << it->second.first->f_x << endl; }
+            if(it->second->visited == false) { cout << it->second->name << " com F(x) = " << it->second->f_x << endl; }
         }
 
-        cout << endl;
+        cout << endl << "De " << city << " escolhi ir para: ";
         
-        // Percorrer o map opened_nodes e checar qual o Nó com menor F(x) -> Esse será o próximo Nó a ser visitado
+        // Finding the opened node with the smaller F(x)
         for (auto it = opened_nodes.begin(); it != opened_nodes.end(); ++it){
-
-            if((it->second.first->f_x < smaller_f) && (it->second.first->visited == false)){
-
-                smaller_f = it->second.first->f_x;
-
-                // Salvando o nome do Nó com menor F(x)
+            if((it->second->f_x < smaller_f) && (it->second->visited == false)){
+                smaller_f = it->second->f_x;
                 city = it->first;
-
             }
-
         }
 
-        // Printando o próximo Nó a ser visitado -> O Nó com menor F(x) dentre os nós abertos
-        cout << "Escolhi ir para " << city << " com F(x) = " << smaller_f << endl;
-        //cout << "\n" << "\n";
+        // Printing next city to be visited
+        cout << city << endl << endl;
 
-        // Agora já sabemos qual Nó será o próximo Nó a ser visitado -> Logo, marcamos esse Nó como visitado
+        // Marking the next city as visited
         auto next_node = opened_nodes.find(city);
-        next_node->second.first->visited = true;
+        next_node->second->visited = true;
 
-        // Testa se eu cheguei em Bucareste
+        // Test if we have reached Bucareste
         if(city == "Bucareste"){
+
+            cout << "Cheguei em Buscareste pelo melhor caminho!!!" << endl << endl;
 
             isDestiny = true;
 
-            // TO-DO: Arrumar estrutura de dados que irá salvar o caminho ótimo
-            // Percorro todos os Nós visitados para montar o caminho mais rápido para ser printado
+            // Logic to print the fastest way
             bool complete_way = false;
             auto city_it = opened_nodes.find("Bucareste");
+            int final_cost = city_it->second->f_x;
             stack<string> fastest_way;
-            fastest_way.push(city); // Adicionando bucareste na pilha
+            string previous_node;
+            fastest_way.push(city); // Adding Bucareste in the stack
 
-            ///////////////////////////////////////////////////////////////////////////////////
-            // debug mode
-            
-            /*
-                best way
-                Arad | Sibiu | Rimnicu Vilcea | Pitesti | Bucareste
-            */ 
-            // cout << "city_it->first: " << city_it->first << endl;
-            // while(complete_way == false){
-            for(int i = 0; i<6; i++){
-                string previous_node = city_it->second.first->previous_node;
+            while(complete_way == false){
+
+                previous_node = city_it->second->previous_node;
                 fastest_way.push(previous_node);
                 city_it = opened_nodes.find(previous_node);
-                
-                // Percorrer o map opened_nodes e ver o que tem 
-                // for (auto it = opened_nodes.begin(); it != opened_nodes.end(); ++it){
-                //     cout << "it->second.first->previous_node " << it->second.first->previous_node << endl;
-                // }
-                // cout << endl;
-                // cout << "previus_node: " << previous_node << endl;
-                // cout << "city_it->first: " << city_it->first << endl;
-                // cout << "city_it->second.first->previous_node: " << city_it->second.first->previous_node << endl;
-                if(city_it->second.first->previous_node == "Arad"){
+
+                if(city_it->second->previous_node == "Arad"){
+                    fastest_way.push(city_it->second->previous_node);
                     complete_way = true;
                 }
                 
-                // city_it = opened_nodes.find(city_it->second.first->previous_node);
-
-                /*
-                    A impressao que tenho eh que falta informacao sobre o anterior do anterior
-                    Desse jeito estamos entrando em loop
-
-                    tem que fazer alguma coisa antes com city_it para que o anterior
-                    de Rimnicu Vilcea seja Sibiu e nao Pitesti
-                */
             }
 
-            ///////////////////////////////////////////////////////////////////////////////////
-
-            // Printando o caminho ótimo
-            cout << "O melhor caminho para chegar em Bucareste: ";
+            // Printing the best way
+            cout << "Melhor Caminho: ";
             while (!fastest_way.empty()){
-                cout << fastest_way.top() << " | ";
+                if(fastest_way.top() == "Bucareste") { cout << fastest_way.top() << " ( Custo = " << final_cost << " )"<< endl; }
+                else { cout << fastest_way.top() << " -> "; }
                 fastest_way.pop();
             }
 
@@ -317,16 +243,10 @@ int main(int argc, char* argv[]){
 
         }
 
-        // O fato de sempre escolher o Nó com o menor F(x) dentre os Nós abertos para ser o próximo Nó visitado, garante que
-        // se eu eventualmente chegar em Bucareste eu vou chegar pelo melhor caminho possível (Caminho Ótimo)
-
-        // Revisar se o map opened_nodes precisa realmente do pair uma vez que F(x) está sendo salvo como atributo do objeto Node
-        // Revisar se estamos usando estruturas de dados a mais do que o necessário
-
     }
 
 
-    // Deletando os nós para liberar o espaço alocado na memória
+    // Deleting the nodes to free the memory
     for(auto it = nodes.begin(); it != nodes.end(); ++it){
         deleteNode(it->second);
     }
@@ -335,6 +255,7 @@ int main(int argc, char* argv[]){
 
 }
 
+// Methods Implementation
 void fillCityWithBuffer(char *city, char *buffer){ // Can be 
         //variables
         int index = 0;
@@ -368,6 +289,11 @@ int getDistanceFromBuffer(int index, char *buffer){
     //cleaning buffer
     sprintf(buffer, "______________\0");
     return distance;
+}
+
+Node* createNode(string name){
+    Node* node = new Node(name);
+    return node;
 }
 
 void deleteNode(Node* node){
