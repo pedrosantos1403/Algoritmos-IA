77209c073cca7ed73be65fd4ccaea72671a3f8aa
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2023-05-13 18:33:09 -0300

    adjusts on training base, teste base and weights

--- Perceptron/Notas sobre arquitetura do perceptron.c
+++ Perceptron/Notas sobre arquitetura do perceptron.c
@@ -2,10 +2,10 @@
 // Iris-versicolor = 010 
 // Iris-virginica = 100
 
-a base de dados 150 plantas sendo:
-	|_ 50 Iris-setosa 
-	|_ 50 Iris-versicolor 
-	|_ 50 Iris-virginica 
+// a base de dados 150 plantas sendo:
+// 	|_ 50 Iris-setosa 
+// 	|_ 50 Iris-versicolor 
+// 	|_ 50 Iris-virginica 
 
 // a => taxa de aprendizagem
 // a = 0.1 
@@ -17,31 +17,31 @@ a base de dados 150 plantas sendo:
 // E é o erro quadrático da época, deve ser plotado
 // bn é o bias associado ao neurônio n
 
-wnm => peso associado ao neurônio n, para entrada m, sendo n = {1,2,3} e m = {1,2,3,4}
-	xm => entrada m = {1,2,3,4}
-		1 -> sepal_length
-		2 -> sepal_width
-		3 -> petal_length
-		4 -> petal_width
+// wnm => peso associado ao neurônio n, para entrada m, sendo n = {1,2,3} e m = {1,2,3,4}
+// 	xm => entrada m = {1,2,3,4}
+// 		1 -> sepal_length
+// 		2 -> sepal_width
+// 		3 -> petal_length
+// 		4 -> petal_width
 
 yn = função de ativação para neurônio n
 
-E = 0
+E = [0,0,0]
 funcão de ativação escolhida = {degrau, sigmoid}
 qtde_rodar = não se sabe valor a priori, tem que acompanhar a queda do E
 
 //definir base de teste
-a base de treino consiste em 105 plantas sendo
-		|_ 35 Iris-setosa
-		|_ 35 Iris-versicolor
-		|_ 35 Iris-virginica
-		= Isso para que o modelo não se ajuste a apenas 1 tipo de plantas
+// a base de treino consiste em 105 plantas sendo
+// 		|_ 35 Iris-setosa
+// 		|_ 35 Iris-versicolor
+// 		|_ 35 Iris-virginica
+// 		= Isso para que o modelo não se ajuste a apenas 1 tipo de plantas
 
 //definir base de treino
-a base de teste consiste em 45 pantas sendo:
-	|_ 15 Iris-setosa
-	|_ 15 Iris-versicolor
-	|_ 15 Iris-virginica
+// a base de teste consiste em 45 pantas sendo:
+// 	|_ 15 Iris-setosa
+// 	|_ 15 Iris-versicolor
+// 	|_ 15 Iris-virginica
 
 // wnm = valores aleatórios entre -1 e 1
 // bn = valores aleatórios entre -1 e 1
--- Perceptron/main.py
+++ Perceptron/main.py
@@ -8,7 +8,13 @@ VERSICOLOR = '010'
 VIRGINICA = '100'
 ALFA = 0.1
 
+# one neuron per class of plant
 NEURON_MAX_NUMBER = 3
+
+# sepal_length, sepal_width, petal_length, petal_width
+PROPERTIES_MAX_NUMBER = 4 
+
+def 
 
 def main():
     io_base = pd.DataFrame(pd.read_csv('Iris_Data.csv'))
@@ -32,29 +38,26 @@ def main():
     # weights
     w = Matrix()
     for i in range(NEURON_MAX_NUMBER):
-        w.insert_row([random.random() for x in range(NEURON_MAX_NUMBER)])
+        w.insert_row([random.random() for x in range(PROPERTIES_MAX_NUMBER)])
 
     # recover species from base
     setosa_base = io_base.loc[io_base['species'] == "Iris-setosa"]
     versicolor_base = io_base.loc[io_base['species'] == "Iris-versicolor"]
     virginica_base = io_base.loc[io_base['species'] == "Iris-virginica"]
     
-    # recover train base
-    setosa_train = setosa_base.sample(frac=0.7,random_state=50)
-    versicolor_train = versicolor_base.sample(frac=0.7,random_state=50)
-    virginica_train = virginica_base.sample(frac=0.7,random_state=50)
-    train_base = [setosa_train, versicolor_train, virginica_train]
-    train_base = pd.concat(train_base)
-    # train_base = setosa_train.append(versicolor_train, ignore_index=True)
-    # train_base = train_base.append(virginica_train, ignore_index=True)
-    print(train_base)
-    print(len(train_base))
+    # recover training base
+    setosa_training = setosa_base.sample(frac=0.7,random_state=50)
+    versicolor_training = versicolor_base.sample(frac=0.7,random_state=50)
+    virginica_training = virginica_base.sample(frac=0.7,random_state=50)
+    training_base = [setosa_training, versicolor_training, virginica_training]
+    training_base = pd.concat(training_base)
     
     # recover test base
-    # test_setosa = setosa_base.drop(train_setosa.index)
-    # test_setosa = setosa_base.drop(train_setosa.index)
-    # test_setosa = setosa_base.drop(train_setosa.index)
-    
+    setosa_test = setosa_base.drop(setosa_training.index)
+    versicolor_test = versicolor_base.drop(versicolor_training.index)
+    virginica_test = virginica_base.drop(virginica_training.index)
+    test_base = [setosa_test, versicolor_test, virginica_test]
+    test_base = pd.concat(test_base)
 
     # while E_past > E_current: #after set train base
         # io = [5.1         ,3.5        ,1.4         ,0.2        ]
